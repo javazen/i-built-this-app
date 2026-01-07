40f67c48bc5630a5f7ba223fff6094db23a5263e
Sam Ragucci <samr742@gmail.com>
2026-01-07 11:41:07 -0800

    outer structure for Featured Products

--- src/components/landing-page/featured-products.tsx
+++ src/components/landing-page/featured-products.tsx
@@ -2,6 +2,26 @@ import { ArrowUpRightIcon, StarIcon } from "lucide-react";
 import SectionHeader from "@/components/common/section-header";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import ProductCard from "../products/product-card";
+
+const featuredProductsData = [
+  {
+    id: 1,
+    name: "ParityKit",
+    description: "A toolkit for creating parity products",
+    tags: ["SaaS", "Pricing", "Global"],
+    votes: 619,
+    isFeatured: true,
+  },
+  {
+    id: 2,
+    name: "New Course",
+    description: "Learn to build production-ready full-stack apps with Next.js",
+    tags: ["Next.js", "Full-stack", "Course"],
+    votes: 124,
+    isFeatured: false,
+  },
+]
 
 export default function FeaturedProducts() {
   return (
@@ -23,6 +43,11 @@ export default function FeaturedProducts() {
               View All
             </Link>
           </Button>
+        </div>
+        <div className="grid-wrapper">
+          {featuredProductsData.map((product) => (
+            <ProductCard key={product.id} product={product} /> 
+          ))}
         </div>
       </div>
     </section>
