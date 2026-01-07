6479974e31155ef32b29ca37fc596ccbf40f39ab
Sam Ragucci <samr742@gmail.com>
2026-01-07 15:59:00 -0800

    Add overview cards for the Featured section

--- src/components/products/product-card.tsx
+++ src/components/products/product-card.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card";
+import { Badge } from "../ui/badge";
+import { StarIcon } from "lucide-react";
 
 interface Product {
     id: number;
@@ -10,5 +14,36 @@ interface Product {
 
 export default function ProductCard({ product }: { 
   product: Product }) {
-  return <div>{product.name}</div>;
-}
+  return (
+    <Link href={`/products/${product.id}`}>
+      <Card className="group card-hover hover:bg-primary-foreground/10
+       border-solid border-gray-400 min-h-[200px]">
+        <CardHeader className="flex-1">
+          <div className="flex items-start gap-4">
+            <div className="flex-1 min-w-0">
+              <div className="flex items-center gap-2">
+                <CardTitle className="text-lg group-hover:text-primary transition-colors">
+                  {product.name}
+                </CardTitle>
+                {product.isFeatured && (
+                  <Badge className="gap-1 bg-primary text-primary-foreground">
+                    <StarIcon className="size-3 fill-current" />
+                    Featured
+                  </Badge>
+                )}
+              </div>
+              <CardDescription>{product.description}</CardDescription>
+            </div>
+            {/** voting buttons */}
+          </div>
+        </CardHeader>
+        <CardFooter>
+          <div className="flex items-center gap-2">
+            {product.tags.map((tag) => (
+              <Badge key={tag}>{tag}</Badge>
+            ))}
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  )  }
