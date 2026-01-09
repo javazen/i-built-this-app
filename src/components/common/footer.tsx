7494369a67753f2cde440a8d53a74ea9bfb31eca
Sam Ragucci <samr742@gmail.com>
2026-01-08 19:42:14 -0800

    Add Text and background to Footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+  <footer className="border-t bg-muted/20 py-12">
+    iBuiltThis Inc. All rights reserved
+  </footer>
+  );
 }
