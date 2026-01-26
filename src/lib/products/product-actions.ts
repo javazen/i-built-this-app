5fce0f126430d79c522850d18eb03ac910a2ae26
Sam Ragucci <samr742@gmail.com>
2026-01-26 11:33:25 -0800

    check auth and zod validation when adding product

--- src/lib/products/product-actions.ts
+++ src/lib/products/product-actions.ts
@@ -1,16 +1,59 @@
 "use server";
 
+import { auth } from "@clerk/nextjs/server";
+import { productSchema } from "./product-validations";
+
 type FormState = {
   success: boolean;
   errors?: Record<string, string[]>
   message: string;
 }
 
-export const addProductAction = async (prevState: FormState, formData: FormData) => {
+export const addProductAction = async (
+  prevState: FormState, 
+  formData: FormData
+): Promise<FormState> => {
   console.log(formData);
+
+  try {
+    const { userId } = await auth();
+    if (!userId) {
+      return {
+        success: false,
+        errors: {},
+        message: "You must be signed in to submit a product!"
+      }
+    }
+
+    // validate the data
+    const rawFormData = Object.fromEntries(formData.entries());
+    const validatedData = productSchema.safeParse(rawFormData);
+    if (!validatedData.success) {
+      console.log(validatedData.error.flatten());
+      return {
+        success: false,
+        errors: validatedData.error.flatten().fieldErrors,
+        message: "Invalid data"
+      }
+    } 
+    const data = validatedData.data;
+
+
+
+  } catch (error) {
+    console.error(error);
+    const errors = error as Record<string, string[]>;
+    return {
+      success: false,
+      errors: errors,
+      message: "Failed to submit product!"
+    }
+
+  }
 
   return {
     success: true,
+    errors: {},
     message: "Product submitted successfully!"
   }
 }
