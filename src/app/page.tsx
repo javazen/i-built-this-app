a2fce1f2b60c0efc1e251fa85ab3cc5c7016f411
Sam Ragucci <samr742@gmail.com>
2026-02-05 21:53:30 -0800

    Add loading skeleton for Recently Launched section
    uses shadcn Skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedProducts from "@/components/landing-page/featured-products";
 import HeroSection from "@/components/landing-page/hero-section";
 import RecentlyLaunchedProducts from "@/components/landing-page/recently-launched-products";
+import ProductSkeleton from "@/components/products/product-skeleton";
 import { Suspense } from "react";
 
 export default function Home() {
@@ -8,7 +9,11 @@ export default function Home() {
     <div>
       <HeroSection />
       <FeaturedProducts />
-      <Suspense fallback={<div>Loading Recently Launched...</div>}>
+      <Suspense 
+        fallback={
+          <ProductSkeleton />
+        }
+      >
         <RecentlyLaunchedProducts />
       </Suspense>
     </div>
