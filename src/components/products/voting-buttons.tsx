2ae6e5e42276eff1af06d8d419e8fc354eeda1a2
Sam Ragucci <samr742@gmail.com>
2026-02-08 20:59:40 -0800

    speed up UI with useOptimistic, startTransition

--- src/components/products/voting-buttons.tsx
+++ src/components/products/voting-buttons.tsx
@@ -1,26 +1,40 @@
 "use client";
 import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { downvoteProductAction, upvoteProductAction } from "@/lib/products/product-actions";
+import { useOptimistic, useTransition } from "react";
 
 export default function VotingButtons({
   hasVoted,
-  voteCount,
+  voteCount: initialVoteCount,
   productId
 }: {
   hasVoted: boolean;
   voteCount: number;
   productId: number
 }) {
+  const [optimisticVoteCount, setOptimisticVoteCount] = useOptimistic(
+    initialVoteCount,
+    (currentCount, change:number) => Math.max(0,currentCount + change)
+  );
+
+  const [isPending, startTransition] = useTransition();
+
   const handleUpvote = async () => {
-    const result = await upvoteProductAction(productId);
-    console.log("upvote result", result);
+    startTransition(async () => {
+      setOptimisticVoteCount(1);
+      await upvoteProductAction(productId);
+    });
   };
+
   const handleDownvote = async () => {
-    const result = await downvoteProductAction(productId);
-    console.log("downvote result", result);
+    startTransition(async () => {
+      setOptimisticVoteCount(-1);
+      await downvoteProductAction(productId);
+    });
   };
+  
   return (
     <div
       className="flex flex-col items-center gap-1 shrink-0"
@@ -36,11 +50,12 @@ export default function VotingButtons({
         className={cn("h-8 w-8 text-primary hover:bg-primary/20",
           hasVoted ? "opacity-50 cursor-not-allowed" : "text-destructive"
         )}
+        disabled={isPending}
       >
         <ChevronUpIcon className="size-5" />
       </Button>
       <span className="text-sm font-semibold transition-colors text-foreground">
-        {voteCount}
+        {optimisticVoteCount}
       </span>
       <Button
         onClick={handleDownvote}
@@ -49,6 +64,7 @@ export default function VotingButtons({
         className={cn("h-8 w-8 text-primary hover:bg-primary/20",
           hasVoted ? "opacity-50 cursor-not-allowed" : "text-destructive"
         )}
+        disabled={isPending}
       >
         <ChevronDownIcon className="size-5 text-destructive" />
       </Button>
